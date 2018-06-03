85d6cf05e67d59625c8aa625363a1af0a28458d6
amishra159 <39692777+amishra159@users.noreply.github.com>
2018-06-03 13:43:08 +0530

    directory

---
+++ download.php
@@ -0,0 +1,19 @@
+<?php
+
+  if(!empty($_GET['file'])){
+     $fileName=basename($_GET['file']);
+	 $filePath='files/'.$fileName;
+	 if(!empty($fileName) && file_exists($filePath)){
+	    // Define Headers
+		  header("Cache-Control:public");
+		  header("Content-Description: File Transfer");
+		  header("Content-Disposition:attachment:filename=$fileName");
+		  header("Content-Type: application/zip");
+		  header("Content-Transfer-Encoding: binary");
+		  //read the files
+		  readfile($filePath);
+		  exit;
+		  }
+		  }
+		  ?>
+	 
