2475d6483387c584d4de4ef1fc6c97e6d5bd5d4a
amishra159 <39692777+amishra159@users.noreply.github.com>
2018-07-08 22:00:42 +0530

    Add files via upload

--- script4.js
+++ script4.js
@@ -1,55 +1,32 @@
                                     
-					 var element1 = document.createElement('div');
-                   element1.innerHTML = '<input type="file" id="in1" accept=".tsv" multiple="mulitple">';
-                      var fileInput1 = element1.firstChild;
-					  
-					  
-	
+					  var elmnt = document.createElement('div');
+                   elmnt.innerHTML = '<input type="file" id="bar" accept=".tsv" webkitdirectory multiple >';
+                      var input_1 = elmnt.firstChild;
+                                                   
+   var select_1 = document.getElementById('agr');
 
-                        var fileSelect1 = document.getElementById("agr");
-            
-					   fileInput1.addEventListener('change', function() { // wait for file selection
-                            
-							   
-								var file =fileInput1.files[0];
-							
+input_1.addEventListener('change', function (evnt) {
+  var fileList = [];
+  for (var i = 0,j = input_1.files.length; i<j; i++) {
+  fileList.push(input_1.files[i]);
+  console.log(fileList);
+  }
+  fileList.forEach(function (file, index) {
+    var name=file.name;
+    console.log(name);
+														 
+								
 						
-						var margin = {top: 50, right: 20, bottom: 60, left: 50},
-    width = 520 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+						var margin = {top: 50, right: 20, bottom: 100, left: 300},
+    width = 800 - margin.left - margin.right,
+    height = 520 - margin.top - margin.bottom;
 	
-   
-					 
-
-					 
-
-// set the ranges
-var x = d3.scaleLinear().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
-
-// define the line
-var valueline = d3.line()
-    .x(function(d) { return x(d.label); })
-    .y(function(d) { return y(d.Time); });
-
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("#chart4").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-		  
-		  var div = d3.select("#floatleft").append("div")	
-    .attr("class", "tooltip")				
-    .style("opacity", 0);
-
-		  
-		  
-		  
-		  
+ 
+                                                        if ($('#chart4 svg').length === 1 ){  
+                                                     $('#chart4 svg:last').remove();
+					                                                                    }
+						  
+ 
 		  
 		  var x1 = d3.scaleBand()
           .range([0, width])
@@ -68,6 +45,9 @@ var svg1 = d3.select("#chart4").append("svg")
           "translate(" + margin.left + "," + margin.top + ")");
 		  
 
+		   
+
+		  
 
 		     var color = d3.scaleOrdinal(["#0d0887", "#6a00a8","#cb4679", "#e16462","#fca636", "#fcce25"]); 
 		  
@@ -76,91 +56,15 @@ var svg1 = d3.select("#chart4").append("svg")
 	
 		  
 
-// Get the data
-d3.tsv("Data/" +file.name, function(error, data) {
+d3.tsv("Data/"+ name, function(error, data) {
   if (error) throw error;
 
-  // format the data
-  data.forEach(function(d) {
-      d.label = +d.label;
-      d.Time = +d.Time;
-  });
-
-  // Scale the range of the data
-  x.domain(d3.extent(data, function(d) { return d.label; }));
-  y.domain([0, d3.max(data, function(d) { return d.Time; })]);
-
-  // Add the valueline path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("d", valueline);
-
-  // Add the X Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-	  .append("text")
-	  .attr("x", 300)
-	  .attr("y", 40)
-      .attr("fill", "Black")
-	   .style("font-family","Times New Roman")
-	    .style("font-size","20px")
-	  .text("TimeStamp");
-
-  // Add the Y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y))
-	  .append("text")
-	  .attr("transform", "rotate(-90)")
-      .attr("y", 20)
-	  .attr("x",-200)
-      .attr("dy", "0.71em")
-      .attr("fill", "Black")
-       .style("font-family","Times New Roman")
-	   .style("font-size","20px")
-	  .text("Time");
-	  
-	  
-	  svg.selectAll("dot")	  
-        .data(data)			
-    .enter().append("circle")								
-        .attr("r", 5)		
-        .attr("cx", function(d) { return x(d.label); })		 
-        .attr("cy", function(d) { return y(d.Time); })		
-        .on("click", function(d) {		
-            div.transition()		
-                .duration(500)		
-                .style("opacity", .9);		
-            div	.html(d.Call + "<br/>"  + d.Time)
-                .style("color","black")	
-                 .style("margin","50px")				 
-                .style("background-color","lightsteelblue")
-                 .style("font-size","20px")	
-                 .style("font-family","times new roman")
-                 .style("text-align","center")
-                  .style("width","100px")				  
-               .style("left", (d3.event.floatleft -100) + "px")		
-                .style("top", (d3.event.pageY +40) + "px");
-            })					
-        .on("mouseout", function(d) {		
-            div.transition()		
-                .duration(100)		
-                .style("opacity", 0);	
-        });
-
-	  
-	  
-	  
-	          			
-});
+                       
+					   
 
 
 
-d3.tsv("Data/"+ file.name, function(error, data) {
-  if (error) throw error;
-
-  // format the data
+ // format the data
   data.forEach(function(d) {
     d.Time = +d.Time;
   });
@@ -194,12 +98,56 @@ d3.tsv("Data/"+ file.name, function(error, data) {
   // add the x Axis
   svg1.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x1));
+      .call(d3.axisBottom(x1))
+	  .append("text")
+	  .attr("x", 200)
+	  .attr("y", 50)
+      .attr("fill", "Black")
+	   .style("font-family","Times New Roman")
+	    .style("font-size","20px")
+	  .text("Label");
+	  
+	  
 
   // add the y Axis
   svg1.append("g")
-      .call(d3.axisLeft(y1));
+      .call(d3.axisLeft(y1))
+	  .append("text")
+	  .attr("transform", "rotate(-90)")
+      .attr("y", -70)
+	  .attr("x",-200)
+      .attr("dy", "0.71em")
+      .attr("fill", "Black")
+       .style("font-family","Times New Roman")
+	   .style("font-size","20px")
+	  .text("Time");
 	  
+	   
+	  
+	   var div=d3.select("#floatleft").append("div");
+	  
+	  svg1.selectAll(".bar")
+			       .data(data)
+				 .on("click", function(d){
+	                     div.transition()		
+                .duration(200)		
+                .style("opacity", .9);		
+            div	.html("Label:" + d.Call + "<br/>" + "Site:" + d.Site + "<br/>"+  "MPI:" + d.MPI+ "<br/>" + "Time:" + d.Time+ "<br/>"+ "App:" + d.App )
+                .style("color","black")	
+                 .style("margin","50px")				 
+                .style("background-color","lightsteelblue")
+                 .style("font-size","20px")	
+                 .style("font-family","times new roman")
+                 .style("text-align","center")
+                  .style("width","200px")	
+                 		  
+				  
+            })					
+				 .on("mouseout", function(d) {
+                    div.transition()		
+                .duration(10000)		
+                .style("opacity", 0);	
+        });
 	 
 	 
 	 
@@ -228,12 +176,12 @@ d3.tsv("Data/"+ file.name, function(error, data) {
           .attr('y', legendRectSize - legendSpacing)              
           .text(function(d) { return d.Call; });                       
 	 
-	  svg1.append("text")
-        .attr("transform","translate(200,440)")
+	 svg1.append("text")
+        .attr("transform","translate(200,445)")
         .attr("text-anchor", "middle")  
         .style("font-size", "16px") 
         .style("text-decoration", "underline")  
-        .text("Bar Chart showing functions with Time");
+        .text("(Aggregate Data)");
 	  
 	  
 
@@ -242,9 +190,10 @@ d3.tsv("Data/"+ file.name, function(error, data) {
 					
 					   });
 					   
+					  });
 					   
-			fileSelect1.addEventListener("click", function () {  // wait for click on "select a file" button
-    fileInput1.click();
+		select_1.addEventListener("click", function () {  // wait for click on "select a file" button
+    input_1.click();
 });
 
 								 
